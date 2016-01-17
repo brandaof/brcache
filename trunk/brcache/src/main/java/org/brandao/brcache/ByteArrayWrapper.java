0933eaf9eca7b4128cf992c41d3be3a1399fa333
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-01-17 17:06:42 +0000

    #

--- trunk/brcache/src/main/java/org/brandao/brcache/ByteArrayWrapper.java
+++ trunk/brcache/src/main/java/org/brandao/brcache/ByteArrayWrapper.java
@@ -20,47 +20,5 @@ public class ByteArrayWrapper
         this.buffer  = data;
         this.length  = length;
     }
-/*
-    public void writeTo(OutputStream out) throws IOException{
-    	this.buffer.write(out, 0, length);
-    }
-
-    public RegionMemory toByteArray(){
-        return this.buffer;
-    }
-
-	public long getId() {
-		return id;
-	}
-
-	public int getSegment() {
-		return segment;
-	}
-
-	public int getLength() {
-		return length;
-	}
-
-	@Override
-    public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + Arrays.hashCode(buffer.getSegments());
-            return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-            if (this == obj)
-                    return true;
-            if (obj == null)
-                    return false;
-            if (getClass() != obj.getClass())
-                    return false;
-            ByteArrayWrapper other = (ByteArrayWrapper) obj;
-            if (!Arrays.equals(buffer.getSegments(), other.buffer.getSegments()))
-                    return false;
-            return true;
-    }
-*/
+    
 }
