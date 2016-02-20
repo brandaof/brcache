7087e1f9a823596aedd9da88af48443dafe88037
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-02-20 18:29:32 +0000

    #

--- trunk/brcache/src/main/java/org/brandao/brcache/collections/fileswapper/IndexKey.java
+++ trunk/brcache/src/main/java/org/brandao/brcache/collections/fileswapper/IndexKey.java
@@ -7,30 +7,47 @@ public class IndexKey implements Serializable{
 
 	private static final long serialVersionUID = 3595347631812335263L;
 
-	public static int MIN_CHARGROUP    = 0x5b;
-
-	public static int MAX_CHARGROUP    = 0x7d;
+	private static final int[] charMap = new int[256];
 	
-	public static int LEN_CHARGROUP    = MAX_CHARGROUP - MIN_CHARGROUP;
+	static{
+		
+		for(int i=0;i<charMap.length;i++){
+			charMap[i] = -1;
+		}
+		
+		int id = 0;
+		
+		for(int i='!';i<='@';i++){
+			int index = i & 0xff;
+			charMap[index] = id++;
+		}
 
-	public static int MIN_NUMBERGROUP  = 0x21;
+		for(int i='[';i<='~';i++){
+			int index = i & 0xff;
+			charMap[index] = id++;
+		}
 
-	public static int MAX_NUMBERGROUP  = 0x3f;
-	
-	public static int LEN_NUMBERGROUP  = MAX_NUMBERGROUP - MIN_NUMBERGROUP;
-	
-	public static int MIN_CHAR2GROUP    = 0xe0;
+		for(int i='à';i<='ö';i++){
+			int index = i & 0xff;
+			charMap[index] = id++;
+		}
 
-	public static int MAX_CHAR2GROUP    = 0xff;
-	
-	public static int LEN_CHAR2GROUP    = MAX_CHAR2GROUP - MIN_CHAR2GROUP;
-	
-	public static int LEN_NODES        = LEN_NUMBERGROUP + LEN_CHARGROUP + LEN_CHAR2GROUP; 
+		for(int i='ù';i<='ý';i++){
+			int index = i & 0xff;
+			charMap[index] = id++;
+		}
+		
+		charMap['ÿ' & 0xff] = id++;
+		
+		LEN_NODES = id;
+	}
 
-	public static int DATA_SIZE        = LEN_NODES*8 + 18; 
+	public static final int LEN_NODES; 
+	
+	public static final int DATA_SIZE  = LEN_NODES*8 + 18; 
 	
 	public static final long MAX_NODES = Long.MAX_VALUE / DATA_SIZE;
-
+	
 	private long id;
 	
 	private long reference;
@@ -50,44 +67,24 @@ public class IndexKey implements Serializable{
 	}
 	
 	public void setNextNode(char id, IndexKey node){
-		int index = id & 0xff;
+		int index     = id & 0xff;
+		int charMapId = charMap[index];
 		
-		if(index < MIN_CHARGROUP && index < MIN_NUMBERGROUP && index < MIN_CHAR2GROUP)
+		if(charMapId < 0)
 			throw new IllegalArgumentException("invalid char: " + id);
 		
-		if(index > MAX_CHARGROUP && index > MAX_NUMBERGROUP &&  index > MAX_CHAR2GROUP)
-			throw new IllegalArgumentException("invalid char: " + id);
-
-		if(index <= MAX_NUMBERGROUP)
-			index = index - MIN_NUMBERGROUP;
-		else
-		if(index <= MAX_CHARGROUP)
-			index = LEN_NUMBERGROUP + (index - MIN_CHARGROUP);
-		else
-			index = LEN_NUMBERGROUP + LEN_CHARGROUP + (index - MIN_CHAR2GROUP);
-		
-		this.nextNodes[index] = node.id;
-		this.needUpdate       = true;
+		this.nextNodes[charMapId] = node.id;
+		this.needUpdate           = true;
 	}
 	
 	public IndexKey getNextNode(char id, SimpleIndexEntityFile session) throws IOException{
-		int index = id & 0xff;
+		int index     = id & 0xff;
+		int charMapId = charMap[index];
 		
-		if(index < MIN_CHARGROUP && index < MIN_NUMBERGROUP && index < MIN_CHAR2GROUP)
+		if(charMapId < 0)
 			throw new IllegalArgumentException("invalid char: " + id);
 		
-		if(index > MAX_CHARGROUP && index > MAX_NUMBERGROUP &&  index > MAX_CHAR2GROUP)
-			throw new IllegalArgumentException("invalid char: " + id);
-
-		if(index <= MAX_NUMBERGROUP)
-			index = index - MIN_NUMBERGROUP;
-		else
-		if(index <= MAX_CHARGROUP)
-			index = LEN_NUMBERGROUP + (index - MIN_CHARGROUP);
-		else
-			index = LEN_NUMBERGROUP + LEN_CHARGROUP + (index - MIN_CHAR2GROUP);
-		
-		long nexNode = this.nextNodes[index];
+		long nexNode = this.nextNodes[charMapId];
 		
 		if(nexNode != -1){
 			session.seek(nexNode);
