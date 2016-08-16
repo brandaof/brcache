8fddf2879cccabc5027e666d8c526a8c88434751
Afonso Brandao <brandaof@users.noreply.github.com>
2016-08-16 13:44:02 +0000

    #

--- src/main/java/org/brandao/brcache/Cache.java
+++ src/main/java/org/brandao/brcache/Cache.java
@@ -48,56 +48,54 @@ public class Cache
     	this.locks = new NamedLock();
     }
     
-	public long size() {
-		return super.getCountRemoved() - super.getCountWrite();
-	}
-
-	public boolean isEmpty() {
-		return super.getCountWrite() == 0;
-	}
-
-	public boolean containsKey(String key) throws RecoverException {
-		return super.getStream(key) != null;
-	}
-
-	public Object get(String key) throws RecoverException {
+	/* métodos de armazenamento */
+	
+	public Object replace(String key, Object value, 
+			long maxAliveTime) throws StorageException {
+		
+		Serializable refLock = this.locks.lock(key);
 		try{
-			InputStream in = super.getStream(key);
-			if(in != null){
-					ObjectInputStream oin = new ObjectInputStream(in);
-					return oin.readObject();
+			Object o = this.get(key);
+			if(o != null){
+				this.put(key, value, maxAliveTime);
+				return true;
 			}
 			else
-				return null;
-		}
-		catch(RecoverException e){
-			throw e;
-		}	
-		catch(Throwable e){
-			throw new RecoverException(e);
-		}	
-	}
-
-	public void put(String key, Object value, long maxAliveTime) throws StorageException {
-		try{
-			ByteArrayOutputStream bout = new ByteArrayOutputStream();
-			ObjectOutputStream oout = new ObjectOutputStream(bout);
-			oout.writeObject(value);
-			oout.flush();
-			this.put(key, maxAliveTime, new ByteArrayInputStream(bout.toByteArray()));
-		}
-		catch(StorageException e){
-			throw e;
+				return false;
 		}
 		catch(Throwable e){
 			throw new StorageException(e);
 		}
+		finally{
+			if(refLock != null){
+				this.locks.unlock(refLock, key);
+			}
+		}
 	}
-
-	public boolean remove(String key) throws RecoverException {
-		return super.remove(key);
+	
+	public boolean replace(String key, Object oldValue, 
+			Object newValue, long maxAliveTime) throws StorageException {
+		
+		Serializable refLock = this.locks.lock(key);
+		try{
+			Object o = this.get(key);
+			if(o != null && o.equals(oldValue)){
+				this.put(key, newValue, maxAliveTime);
+				return true;
+			}
+			else
+				return false;
+		}
+		catch(Throwable e){
+			throw new StorageException(e);
+		}
+		finally{
+			if(refLock != null){
+				this.locks.unlock(refLock, key);
+			}
+		}
 	}
-
+	
 	public Object putIfAbsent(String key, 
 			Object value, long maxAliveTime) throws StorageException {
 		
@@ -122,6 +120,44 @@ public class Cache
 			}
 		}
 	}
+	
+	public void put(String key, Object value, long maxAliveTime) throws StorageException {
+		try{
+			ByteArrayOutputStream bout = new ByteArrayOutputStream();
+			ObjectOutputStream oout = new ObjectOutputStream(bout);
+			oout.writeObject(value);
+			oout.flush();
+			this.put(key, maxAliveTime, new ByteArrayInputStream(bout.toByteArray()));
+		}
+		catch(StorageException e){
+			throw e;
+		}
+		catch(Throwable e){
+			throw new StorageException(e);
+		}
+	}
+
+	/* métodos de coleta*/
+	
+	public Object get(String key) throws RecoverException {
+		try{
+			InputStream in = super.getStream(key);
+			if(in != null){
+					ObjectInputStream oin = new ObjectInputStream(in);
+					return oin.readObject();
+			}
+			else
+				return null;
+		}
+		catch(RecoverException e){
+			throw e;
+		}	
+		catch(Throwable e){
+			throw new RecoverException(e);
+		}	
+	}
+
+    /* métodos de remoção */
 
 	public boolean remove(String key, Object value) throws StorageException {
 		
@@ -143,51 +179,23 @@ public class Cache
 			}
 		}
 	}
-
-	public boolean replace(String key, Object oldValue, 
-			Object newValue, long maxAliveTime) throws StorageException {
-		
-		Serializable refLock = this.locks.lock(key);
-		try{
-			Object o = this.get(key);
-			if(o != null && o.equals(oldValue)){
-				this.put(key, newValue, maxAliveTime);
-				return true;
-			}
-			else
-				return false;
-		}
-		catch(Throwable e){
-			throw new StorageException(e);
-		}
-		finally{
-			if(refLock != null){
-				this.locks.unlock(refLock, key);
-			}
-		}
+	
+	public boolean remove(String key) throws RecoverException {
+		return super.remove(key);
 	}
 
-	public Object replace(String key, Object value, 
-			long maxAliveTime) throws StorageException {
-		
-		Serializable refLock = this.locks.lock(key);
-		try{
-			Object o = this.get(key);
-			if(o != null){
-				this.put(key, value, maxAliveTime);
-				return true;
-			}
-			else
-				return false;
-		}
-		catch(Throwable e){
-			throw new StorageException(e);
-		}
-		finally{
-			if(refLock != null){
-				this.locks.unlock(refLock, key);
-			}
-		}
+    /* métodos de manipulação*/
+    
+	public long size() {
+		return super.getCountRemoved() - super.getCountWrite();
 	}
 
+	public boolean isEmpty() {
+		return super.getCountWrite() == 0;
+	}
+
+	public boolean containsKey(String key) throws RecoverException {
+		return super.getStream(key) != null;
+	}
+	
 }
