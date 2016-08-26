cad8752f0270e6952b41fabc363da48e9453b7eb
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-26 20:34:33 +0000

    #

--- trunk/brcache/src/main/java/org/brandao/brcache/CacheError.java
+++ trunk/brcache/src/main/java/org/brandao/brcache/CacheError.java
@@ -1,24 +1,47 @@
 package org.brandao.brcache;
 
+/**
+ * Descreve um erro no cache. 
+ * @author Brandao.
+ *
+ */
 public class CacheError {
 
 	private final int id;
 	
 	private final String message;
 
+	/**
+	 * Cria um novo erro com uma identificação e mensagem específica.
+	 * @param id identificação.
+	 * @param message mensagem.
+	 */
 	public CacheError(int id, String message) {
 		this.id = id;
 		this.message = message;
 	}
 
+	/**
+	 * Obtém a identificação.
+	 * @return identificação.
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Obtém a mensagem do erro.
+	 * @return mensagem.
+	 */
 	public String getMessage() {
 		return message;
 	}
 	
+	/**
+	 * Obtém a descrição do erro.
+	 * @param params
+	 * @return
+	 */
 	public String getString(Object ... params){
 		return "ERROR " + this.id + ": " + String.format(this.message, (Object[])params);
 	}
