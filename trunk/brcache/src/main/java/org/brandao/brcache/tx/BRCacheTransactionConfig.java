8d32576c4cb12e64b854b87a46bc1ada20b3b46f
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-26 02:32:46 +0000

    #

--- trunk/brcache/src/main/java/org/brandao/brcache/tx/BRCacheTransactionConfig.java
+++ trunk/brcache/src/main/java/org/brandao/brcache/tx/BRCacheTransactionConfig.java
@@ -10,19 +10,19 @@ public class BRCacheTransactionConfig extends BRCacheConfig{
 
 	public void setConfiguration(Configuration config){
 		this.configuration       = config;
-        this.nodesBufferSize     = config.getLong("transaction_nodes_buffer_size",		"3k");
+        this.nodesBufferSize     = config.getLong("transaction_nodes_buffer_size",		"8k");
         this.nodesPageSize       = config.getLong("transaction_nodes_page_size",		"1k");
         this.nodesSwapFactor     = config.getDouble("transaction_nodes_swap_factor",	"0.1");
-        this.indexBufferSize     = config.getLong("transaction_index_buffer_size",		"1k");
+        this.indexBufferSize     = config.getLong("transaction_index_buffer_size",		"8k");
         this.indexPageSize       = config.getLong("transaction_index_page_size",		"512b");
         this.indexSwapFactor     = config.getDouble("transaction_index_swap_factor",	"0.1");
-        this.dataBufferSize      = config.getLong("transaction_data_buffer_size",		"6k");
+        this.dataBufferSize      = config.getLong("transaction_data_buffer_size",		"1m");
         this.dataBlockSize       = config.getLong("transaction_data_block_size",		"64b");
-        this.dataPageSize        = config.getLong("transaction_data_page_size",			"512b");
+        this.dataPageSize        = config.getLong("transaction_data_page_size",			"1k");
         this.dataSwapFactor      = config.getDouble("transaction_data_swap_factor",		"0.1");
         this.maxSizeEntry        = config.getLong("transaction_max_size_entry",			"1m");
-        this.maxSizeKey          = config.getInt("transaction_max_size_key",			"48");
-        this.swapperThread       = config.getInt("transaction_swapper_thread",			"1");
+        this.maxSizeKey          = config.getInt("max_size_key",						"48");
+        this.swapperThread       = 1;
         this.dataPath            = config.getString("data_path",			"/var/brcache") + "/tx";
         this.swapper             = SwaperStrategy.valueOf(config.getString("transaction_swapper_type","FILE").toUpperCase());
     }
