0565d493cdd27272f02ec715db6e3613ca680ff6
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-27 20:01:35 +0000

    #

---
+++ trunk/brcache/src/main/java/org/brandao/brcache/tx/EntryMetaData.java
@@ -0,0 +1,11 @@
+package org.brandao.brcache.tx;
+
+import java.io.Serializable;
+
+public class EntryMetaData implements Serializable{
+
+	private long timeToLive;
+	
+	private long timeToIdle;
+	
+}
