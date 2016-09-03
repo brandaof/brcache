006968f35d7c2c5b76c0dcdc04fe39a18ee2887d
Afonso Brandao <brandaof@users.noreply.github.com>
2016-09-03 03:05:42 +0000

    #

---
+++ src/main/java/org/brandao/brcache/CacheConstants.java
@@ -0,0 +1,35 @@
+package org.brandao.brcache;
+
+public interface CacheConstants {
+
+    public static final String DATA_PATH 			= "data_path";
+	
+    public static final String NODES_BUFFER_SIZE 	= "nodes_buffer_size";
+    
+    public static final String NODES_PAGE_STRING 	= "nodes_page_size";
+    
+    public static final String NODES_SWAP_FACTOR 	= "nodes_swap_factor";
+    
+    public static final String INDEX_BUFFER_SIZE 	= "index_buffer_size";
+    
+    public static final String INDEX_PAGE_SIZE 		= "index_page_size";
+    
+    public static final String INDEX_SWAP_SIZW 		= "index_swap_factor";
+    
+    public static final String DATA_BUFFER_SIZE 	= "data_buffer_size";
+    
+    public static final String DATA_BLOCK_SIZE 		= "data_block_size";
+    
+    public static final String DATA_PAGE_SIZE 		= "data_page_size";
+    
+    public static final String DATA_SWAP_SIZE 		= "data_swap_factor";
+    
+    public static final String MAX_SIZE_ENTRY 		= "max_size_entry";
+    
+    public static final String MAX_SIZE_KEY 		= "max_size_key";
+    
+    public static final String SWAPPER_THREAD 		= "swapper_thread";
+    
+    public static final String SWAPPER_TYPE 		= "swapper_type";
+    
+}
