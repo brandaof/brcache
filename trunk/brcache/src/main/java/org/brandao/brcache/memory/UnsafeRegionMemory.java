10c4f56f1461ebabd34d0b748ead3ec7b6ab303d
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-09-19 20:43:24 +0000

    #

--- trunk/brcache/src/main/java/org/brandao/brcache/memory/UnsafeRegionMemory.java
+++ trunk/brcache/src/main/java/org/brandao/brcache/memory/UnsafeRegionMemory.java
@@ -3,9 +3,9 @@ package org.brandao.brcache.memory;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
-public class UnsafeRegionMemory implements Serializable{
+public class UnsafeRegionMemory 
+	implements RegionMemory{
 
 	private static final long serialVersionUID = 5941866368711530651L;
 
@@ -25,36 +25,36 @@ public class UnsafeRegionMemory implements Serializable{
 		this.off = 0;
 	}
 	
-	public long length(){
+	public long size(){
 		return this.length;
 	}
 	
 	public long read(byte[] buf, long off, long len){
 		long trans = Math.min(this.length - this.off, len);
-		long buffAdress = Memory.getAddress(buf);
-		Memory.arrayCopy(this.address, this.off, buffAdress, off, trans);
+		long buffAdress = UnsafeMemoryUtil.getAddress(buf);
+		UnsafeMemoryUtil.arrayCopy(this.address, this.off, buffAdress, off, trans);
 		this.off += trans;
 		
 		return trans;
 	}
 	
-	public long read(UnsafeRegionMemory buf, long off, long len){
+	public long read(RegionMemory buf, long off, long len){
 		long trans = Math.min(this.length - this.off, len);
-		Memory.arrayCopy(this.address, this.off, buf.address, off, trans);
+		UnsafeMemoryUtil.arrayCopy(this.address, this.off, ((UnsafeRegionMemory)buf).address, off, trans);
 		this.off += trans;
 		return trans;
 	}
 	
 	public void write(byte[] buf, long off, long len){
-		long buffAdress = Memory.getAddress(buf);
+		long buffAdress = UnsafeMemoryUtil.getAddress(buf);
 		long trans = Math.min(this.length - this.off, len);
-		Memory.arrayCopy(buffAdress, off, this.address, this.off, trans);
+		UnsafeMemoryUtil.arrayCopy(buffAdress, off, this.address, this.off, trans);
 		this.off += trans;
 	}
 
-	public void write(UnsafeRegionMemory buf, long off, long len){
+	public void write(RegionMemory buf, long off, long len){
 		long trans = Math.min(this.length - this.off, len);
-		Memory.arrayCopy(buf.address, off, this.address, this.off, trans);
+		UnsafeMemoryUtil.arrayCopy(((UnsafeRegionMemory)buf).address, off, this.address, this.off, trans);
 		this.off += trans;
 	}
 	
@@ -68,9 +68,9 @@ public class UnsafeRegionMemory implements Serializable{
     }
 
     private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
-    	this.length = stream.readLong();
-    	this.off    = 0;
-		Memory.alloc(length, this);
+    	this.length  = stream.readLong();
+    	this.off     = 0;
+    	this.address = UnsafeMemoryUtil.alloc(length);
 		long len;
 		byte[] b = new byte[9024];
 		while(this.off < this.length){
@@ -88,10 +88,11 @@ public class UnsafeRegionMemory implements Serializable{
 	
     protected void finalize() throws Throwable{
     	try{
-    		Memory.release(this);
+    		UnsafeMemoryUtil.free(this.address);
     	}
     	finally{
     		super.finalize();
     	}
     }
+
 }
