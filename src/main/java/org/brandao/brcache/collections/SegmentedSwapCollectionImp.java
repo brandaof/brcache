b74c7b693a6ad4be824e3cf7eefcdf52acfa964b
afonso <afonso.rbn@gmail.com>
2018-09-09 20:35:31 -0300

    #

--- src/main/java/org/brandao/brcache/collections/SegmentedSwapCollectionImp.java
+++ src/main/java/org/brandao/brcache/collections/SegmentedSwapCollectionImp.java
@@ -64,7 +64,7 @@ public class SegmentedSwapCollectionImp<T>
         this.readOnly            = false;
         this.forceSwap           = true;
         this.live                = true;
-        this.swapCollections     = new SwapCollectionImp[1];//[maxCapacity/MAX_ITENS_PER_SEGMENT + (maxCapacity % MAX_ITENS_PER_SEGMENT != 0? 1 : 0)];
+        this.swapCollections     = new SwapCollectionImp[maxCapacity/MAX_ITENS_PER_SEGMENT + (maxCapacity % MAX_ITENS_PER_SEGMENT != 0? 1 : 0)];
         
         int countMaxCapacity = maxCapacity;
         for(int i=0;i<this.swapCollections.length;i++){
